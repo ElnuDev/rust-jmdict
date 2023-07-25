1792926dfde2cfd9d8b9f47a11543fb154e2a4e0
ElnuDev <elnu@elnu.com>
2023-07-25 10:27:26 -0700

    Add date to JMdict file name

--- data/fetch.sh
+++ data/fetch.sh
@@ -6,5 +6,10 @@ wget http://ftp.edrdg.org/pub/Nihongo/JMdict.gz
 # Extract and delete archive
 gzip -d JMdict.gz
 
+# Get date
+date=`grep -m 1 -oE '<!-- JMdict created: [0-9]{4}-[0-9]{2}-[0-9]{2} -->' JMdict.xml | sed -E 's/<!-- JMdict created: (.+) -->/\1/'`
+
+rm JMdict*.xml
+
 # Add XML extension
-mv JMdict JMdict.xml
+mv JMdict JMdict-${date}.xml
